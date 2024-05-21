d7abeba8018c15a71cced23da3f7ed4526bed951
Arauz <86898345+kjarj54@users.noreply.github.com>
2024-05-21 16:23:21 -0600

    [INIT]

---
+++ Api-de-Challenge-Alura-Foro-Hub/src/main/java/foro/hub/api/Api/de/Challenge/Alura/Foro/Hub/ApiDeChallengeAluraForoHubApplication.java
@@ -0,0 +1,13 @@
+package foro.hub.api.Api.de.Challenge.Alura.Foro.Hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeChallengeAluraForoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeChallengeAluraForoHubApplication.class, args);
+	}
+
+}
---
+++ Api-de-Challenge-Alura-Foro-Hub/src/test/java/foro/hub/api/Api/de/Challenge/Alura/Foro/Hub/ApiDeChallengeAluraForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package foro.hub.api.Api.de.Challenge.Alura.Foro.Hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeChallengeAluraForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
